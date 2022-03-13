99bf85f00164952413d4bc78b50b19e4b05514bd
nonhlanhla <nonhlanhlamahlobo09@gmail.com>
2022-03-13 14:42:42 -0700

    hellow

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return:Always O (success)
+ */
+
+int main(void)
+ {
+	 puts("\"programming is like building a multilingual puzzle");
+	 return (O);
+ } 
