13cd671e2554d379f0b1c64ec3ec148a3c128da8
nonhlanhla <nonhlanhlamahlobo09@gmail.com>
2022-03-15 14:06:22 -0700

    hello

---
+++ 0-putchar.c
@@ -0,0 +1,17 @@
+#include "_putchar.h"
+/**
+ * main - entry point.
+ * Description:program starts and end here.
+ * Return: 0 on success.
+ * /
+ * int main(void)
+ * {
+ 	char text[9] = "putcher";
+	int i;
+	for (i = 0; i < 9; i++
+  	{
+	_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
+	}
