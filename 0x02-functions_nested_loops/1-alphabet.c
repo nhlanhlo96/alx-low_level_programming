f00b175beb49d35e6c1848d31a17c8617c75f72e
nonhlanhla <nonhlanhlamahlobo09@gmail.com>
2022-03-15 16:49:03 -0700

    hello

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+nclude <stdio.h>
+#include "main.h"
+/**
+ *  * print_alphabet - Prints lowercase alphabets
+ *   * Return: void
+ *   @@ -9,15 +9,6 @@ void print_alphabet(void)
+ *   	char alph;
+ *
+ *   		for (alph = 'a'; alph <= 'z'; alph++)
+ *   			putchar(alph);
+ *   				putchar('\n');
+ *   				}
+ */**
+ * main - Entry point
+ *  * Return: Always 0 (Success)
+ *   */
+ int main(void)
+{
+		print_alphabet();
+			return (0);
+				_putchar(alph);
+					_putchar('\n');
+}
